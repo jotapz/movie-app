8747db687c5e0986beb1d7144d35b8abf395d017
João Pedro <jpedros1511@gmail.com>
2025-12-19 12:06:47 -0300

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Pedro Franco on 19/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
