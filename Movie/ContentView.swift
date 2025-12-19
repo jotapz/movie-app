b7ceec64b2192c53d9cc2dbf6ee5ff9ec465df7e
João Pedro <jpedros1511@gmail.com>
2025-12-19 12:06:47 -0300

    Initial Commit

---
+++ Movie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie
+//
+//  Created by Pedro Franco on 19/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
